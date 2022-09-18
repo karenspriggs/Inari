938d046b036ebb387e8b59d72c827e92409cb38f
Karen Spriggs <k.spriggs01@gmail.com>
2022-09-18 16:52:28 -0500

    Basic player stats

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
 public class PlayerData : MonoBehaviour
 {
+    [Header("Player Stats")]
+    [SerializeField]
+    private int maxHP;
+    [SerializeField]
+    private int currentHP;
+    [SerializeField]
+    private int Attack;
+    [SerializeField]
+    private Weapon currentWeapon;
+
+    public static Action<int> PlayerTookDamage; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHP = maxHP;
+        TakeDamage(1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log(damage);
+        currentHP -= damage;
+        PlayerTookDamage?.Invoke(damage);
     }
 }
