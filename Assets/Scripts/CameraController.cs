4dbe5f48f2d100cfe8244cc3da65bc0a17475a38
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2022-09-29 22:36:31 -0500

    Added different camera mechanics to camera
    only added code, needs to only affect add and

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,38 +5,56 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
-
-    public float smoothSpeed = 0.125f;
-    public Vector3 offest;
-
-    public GameObject wall;
-    public Transform place;
+    public Transform[] playerTransform;
 
     private void Start()
     {
-        transform.LookAt(player);
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        playerTransform = new Transform[allPlayers.Length];
+        for (int i = 0; i < allPlayers.Length; i++) 
+        { 
+            playerTransform[i] = allPlayers[i].transform;
+        }
     }
 
-    void FixedUpdate()
+    public float yOffset = 2.0f;
+    public float minDistance = 7.5f;
+
+    private float xMin,xMax, yMin, yMax;
+
+    void LateUpdate()
     {
-        //moving camera with player.
-        Vector3 cameraPosition = player.position + offest;
+        if(playerTransform.Length == 0)
+        {
+            
+            return;
+        }
+        xMin = xMax = playerTransform[0].position.x;
+        yMin = yMax = playerTransform[0].position.y;
+        for (int i = 1; i < playerTransform.Length; i++)
+        {
+            if (playerTransform[i].position.x < xMin) 
+                xMin = playerTransform[i].position.x;
 
-        Vector3 smoothPos = Vector3.Lerp(transform.position, cameraPosition, smoothSpeed);
-        transform.position = smoothPos;
+            if (playerTransform[i].position.x > xMax)
+                xMax = playerTransform[i].position.x;
 
-        
+            if (playerTransform[i].position.x < yMin)
+                yMin = playerTransform[i].position.y;
 
-        //goes back to following player once enimes all die.
-    
+            if (playerTransform[i].position.x > yMax)
+                yMax = playerTransform[i].position.y;
+
+        }
+        float xMiddle = (xMax + xMin) / 2;
+        float yMiddle = (yMax + yMin) / 2;
+        float distance = xMax - xMin;
+        if (distance< minDistance)
+            distance = minDistance;
     }
-        //stops at blocker
+       
     public void OnTriggerEnter(Collider other)
     {
-        
-            smoothSpeed = 0;
-            transform.LookAt(place);
             
             
     }
