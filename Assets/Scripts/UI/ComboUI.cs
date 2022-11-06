c7c13dfb844486de557267f467dddbd87b8e569e
Karen Spriggs <k.spriggs01@gmail.com>
2022-11-06 13:50:21 -0600

    combo color interpolation

--- Assets/Scripts/UI/ComboUI.cs
+++ Assets/Scripts/UI/ComboUI.cs
@@ -9,15 +9,29 @@ public class ComboUI : MonoBehaviour
     public TMP_Text comboText;
     public float tweenIntensity;
     public float tweenTime;
+    
+    float decentComboFraction;
+    float highComboFraction;
+    float highestComboFraction;
+
+    public int decentComboNumber;
+    public int highComboNumber;
+    public int highestComboNumber;
 
     public Color startingColor;
     public Color decentComboColor;
+    public Color highComboColor;
     public Color highestComboColor;
+
+    Color currentColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        decentComboFraction = 1 / decentComboNumber;
+        Debug.Log(decentComboFraction);
+        highComboFraction = 1 / (highComboNumber - decentComboNumber);
+        currentColor = startingColor;
     }
 
     // Update is called once per frame
@@ -37,18 +51,31 @@ public class ComboUI : MonoBehaviour
 
         if (currentScore == 0)
         {
+            currentColor = startingColor;
             comboText.color = startingColor;
         }
 
-        if (currentScore >= 6)
+        if (0 < currentScore && currentScore <= decentComboNumber)
         {
-            comboText.color = decentComboColor;
+            UpdateTextColor(decentComboColor);
         }
 
-        if (currentScore >= 12)
+        if (currentScore <= highComboNumber && currentScore > decentComboNumber)
         {
-            comboText.color = highestComboColor;
+            UpdateTextColor(highComboColor);
         }
+
+        if (currentScore <= highestComboNumber && currentScore > highComboNumber)
+        {
+            UpdateTextColor(highestComboColor);
+        }
+
+    }
+
+    void UpdateTextColor(Color newcolor)
+    {
+        currentColor = Color.Lerp(currentColor, newcolor, 0.167f);
+        comboText.color = currentColor;
     }
 
     void TweenScore()
