0b04c446c158e75dea433547f65b4adf869a1189
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2022-11-16 20:12:32 -0600

    Made the option sliders work and now control the audio

--- Assets/Scripts/AudioAdjusting.cs
+++ Assets/Scripts/AudioAdjusting.cs
@@ -7,9 +7,17 @@ public class AudioAdjusting : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
 
-    public void SetVolume(float sliderValue)
+    public void SetMaster(float sliderValue)
     {
-        audioMixer.SetFloat("Master(music)", Mathf.Log10(sliderValue) * 20);
+        audioMixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetMusic(float sliderValue)
+    {
+        audioMixer.SetFloat("Music", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetSFX(float sliderValue)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
     }
 
 }
