66bae3d3e7894a4bec665bd1174cbc3da30104b0
Karen Spriggs <k.spriggs01@gmail.com>
2022-11-28 00:54:03 -0600

    moving scenes around + enemy room stuff

--- Assets/Scripts/Enemies/EnemyRoom.cs
+++ Assets/Scripts/Enemies/EnemyRoom.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] Walls;
+    public BoxCollider2D[] WallColliders;
+    SpriteRenderer spriteRenderer;
+
+    public bool isActivated;
+    public int enemyCount;
+
+    private void Start()
     {
-        
+        isActivated = false;
+
+        foreach (GameObject wall in Walls)
+        {
+            wall.SetActive(false);
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ActivateRoom()
     {
-        
+        foreach(GameObject wall in Walls)
+        {
+            wall.SetActive(true);
+        }
+
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !isActivated)
+        {
+            isActivated = true;
+            ActivateRoom();
+        }
     }
 }
