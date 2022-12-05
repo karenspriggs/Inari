7ea1b47b0dfe96ae750470c60e113fa71b330684
Karen Spriggs <k.spriggs01@gmail.com>
2022-12-04 21:29:20 -0600

    starting on multi range enemy controller

--- Assets/Scripts/Enemies/MultiRangeEnemyController.cs
+++ Assets/Scripts/Enemies/MultiRangeEnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MultiRangeEnemyStates
+public enum MultiRangeEnemyState
 {
     Idle,
     Wander,
@@ -26,15 +26,91 @@ public class MultiRangeEnemyController : MonoBehaviour
     Rigidbody2D rigidbody;
     CapsuleCollider2D collider;
 
+    public GameObject chaseTarget;
     public GameObject animationProps;
 
     [SerializeField]
-    EnemyState currentState;
+    MultiRangeEnemyState currentState;
+
+    [SerializeField]
+    float attackTimer;
+    float wanderTimer;
+
+    bool canMove = true;
+    bool canAttack = false;
+    bool shouldWander = false;
+    bool shouldAttack = false;
+    bool shouldHitStun = false;
+    bool targetChosen = false;
+    bool isGrounded = true;
+
+    [SerializeField]
+    Vector2 currentWanderTarget;
+    float currentWanderMin;
+    float currentWanderMax;
+    public float lastWanderDistance;
+
+    public bool isFacingRight;
+
+    public float AttackDistance;
+    public float ChaseSpeed;
+    public float WanderSpeed;
+    public float xChaseDistance;
+    public float yChaseDistance;
+    public float xStopDistance;
+    public float yStopDistance;
+    public float attackCooldown;
+    public float WanderCooldown;
+    public int maxWanderDistance;
+    public bool willWander = true;
+    public Vector2 deathLaunchDistance;
+
+    private LayerMask groundMask;
+    private LayerMask platformMask;
+
+    public float groundCheckXDistance = 0.25f;
+    public float groundCheckYDistance = 0.25f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAnimator = GetComponent<EnemyAnimator>();
+        enemyData = GetComponent<EnemyData>();
+        enemyParticles = GetComponent<EnemyParticles>();
+        enemySounds = GetComponent<EnemySound>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
+
+        chaseTarget = GameObject.FindWithTag("Player");
+        currentState = MultiRangeEnemyState.Idle;
+        attackTimer = attackCooldown;
+        wanderTimer = WanderCooldown;
+
+        groundMask = LayerMask.GetMask("Ground");
+        platformMask = LayerMask.GetMask("One-Way Platform");
+    }
+    
+    public void SwitchState(MultiRangeEnemyState newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case (MultiRangeEnemyState.Idle):
+                wanderTimer = WanderCooldown;
+                canMove = true;
+                break;
+            case (MultiRangeEnemyState.Wander):
+                break;
+        }
+    }
+
+    public void DoState(MultiRangeEnemyState state)
+    {
+        switch (state)
+        {
+
+        }
     }
 
     // Update is called once per frame
