2b1930b9edd1b0abaa0f9d1eed7a48370076a0e9
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2022-12-07 16:03:13 -0600

    Added a kill count script
    This is to show up on the stats screen at the end of the level

---
+++ Assets/Scripts/KillCount.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class KillCount : MonoBehaviour
+{
+    public Text counterText;
+    int counter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
